a8daf603f403517fd5f8466bc02092277d9fdf1c
bulutaysarac <bulutaysarac@gmail.com>
2023-05-04 16:23:07 +0300

    DiscountService

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,66 @@
+﻿using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.Extensions;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var discounts = await _discountService.GetAll();
+            return this.CreateRespose(discounts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var discount = await _discountService.GetById(id);
+            return this.CreateRespose(discount);
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/GetByCode/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _sharedIdentityService.GetUserId;
+            var discount = await _discountService.GetByCodeAndUserId(code, userId);
+            return this.CreateRespose(discount);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            var response = await _discountService.Save(discount);
+            return this.CreateRespose(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            var response = await _discountService.Update(discount);
+            return this.CreateRespose(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _discountService.Delete(id);
+            return this.CreateRespose(response);
+        }
+    }
+}
